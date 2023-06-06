ff873f7b5ad0c6eb9eb86ee48f0d661552caab22
arifazfar99 <arifazfar99@gmail.com>
2023-06-06 19:21:05 +0800

    deleted unnecessary files

---
+++ src/Resume.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div>
+      <h1>Arif Azfar Azri</h1>
+      <h3>Frontend Developer</h3>
+
+      <h2>Experience</h2>
+      <ul>
+        <li>Company A - Frontend Developer (2018-2021)</li>
+        <li>Company B - Junior Developer (2016-2018)</li>
+      </ul>
+
+      <h2>Education</h2>
+      <ul>
+        <li>University of XYZ - Bachelor's Degree in Computer Science</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Resume;
